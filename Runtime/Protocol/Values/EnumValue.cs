8ff4c740759dd7551bfd8d97504caf56eb2e0adc
HeaX <heax@moonsama.com>
2023-11-09 17:47:28 +0100

    feat: reorganized folders and added enum value

---
+++ Runtime/Protocol/Values/EnumValue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MultiplayerProtocol
+{
+    public class EnumValue<T> : ISerializableValue<T> where T : struct, Enum
+    {
+        public T value { get; set; }
+
+        public EnumValue()
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException(nameof(value) + " is not an Enum");
+            }
+        }
+
+        public void SerializeInto(SerializedMessage message)
+        {
+            message.Write((int)(object)value);
+        }
+
+        public void DeserializeFrom(SerializedMessage message)
+        {
+            value = (T)(object)message.ReadInt();
+        }
+    }
+}
