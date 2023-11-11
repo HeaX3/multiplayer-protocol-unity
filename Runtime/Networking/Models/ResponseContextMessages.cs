c1adca40f9e57c28645a3d2d748005261390605c
HeaX <heax@moonsama.com>
2023-11-11 15:56:48 +0100

    feat: added compound which can be used to hand over response messages

---
+++ Runtime/Networking/Models/ResponseContextMessages.cs
@@ -0,0 +1,36 @@
+﻿using JetBrains.Annotations;
+
+namespace MultiplayerProtocol
+{
+    public class ResponseContextMessages
+    {
+        [CanBeNull] public SerializedMessages preResponse { get; set; }
+        [CanBeNull] public SerializedMessages postResponse { get; set; }
+
+        public ResponseContextMessages()
+        {
+        }
+
+        public ResponseContextMessages(SerializedMessages preResponse, SerializedMessages postResponse)
+        {
+            this.preResponse = preResponse;
+            this.postResponse = postResponse;
+        }
+
+        public static ResponseContextMessages PreResponse(SerializedMessages messages)
+        {
+            return new ResponseContextMessages
+            {
+                preResponse = messages
+            };
+        }
+
+        public static ResponseContextMessages PostResponse(SerializedMessages messages)
+        {
+            return new ResponseContextMessages
+            {
+                postResponse = messages
+            };
+        }
+    }
+}
