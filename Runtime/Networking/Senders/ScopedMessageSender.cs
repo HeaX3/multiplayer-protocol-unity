25d0cac55dfe23ade0265ee0e7dfac3dc428b438
HeaX <heax@moonsama.com>
2023-11-11 16:59:06 +0100

    feat: added abstract class implementation of scoped message sender

---
+++ Runtime/Networking/Senders/ScopedMessageSender.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MultiplayerProtocol.Senders
+{
+    public abstract class ScopedMessageSender : IScopedMessageSender
+    {
+        public abstract IEnumerable<NetworkConnection> GetConnections();
+
+        public void Send(INetworkMessage message) => ((IScopedMessageSender)this).Send(message);
+    }
+}
