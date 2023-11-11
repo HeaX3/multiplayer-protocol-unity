04a78d99cfca6a51735737c610d2e88156d07351
HeaX <heax@moonsama.com>
2023-11-11 17:08:05 +0100

    feat: added array value type

---
+++ Runtime/Protocol/Values/ArrayValue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MultiplayerProtocol
+{
+    public class ArrayValue<T> : ISerializableValue<T[]> where T : ISerializableValue, new()
+    {
+        public T[] value { get; set; }
+
+        public void SerializeInto(SerializedData message)
+        {
+            var length = this.value?.Length ?? 0;
+            message.Write(length);
+            if (this.value == null || length == 0) return;
+            foreach (var value in this.value)
+            {
+                value.SerializeInto(message);
+            }
+        }
+
+        public void DeserializeFrom(SerializedData message)
+        {
+            var length = message.ReadInt();
+            if (length == 0)
+            {
+                this.value = Array.Empty<T>();
+                return;
+            }
+
+            var value = new T[length];
+            for (var i = 0; i < length; i++)
+            {
+                var entry = new T();
+                entry.DeserializeFrom(message);
+                value[i] = entry;
+            }
+
+            this.value = value;
+        }
+    }
+}
