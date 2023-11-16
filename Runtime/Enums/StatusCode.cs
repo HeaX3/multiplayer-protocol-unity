27d910d4afe0a4dbfb141de951f1f6084102b8f4
HeaX <heax@moonsama.com>
2023-11-16 13:25:56 +0100

    feat: added rejected exception

--- Runtime/Enums/StatusCode.cs
+++ Runtime/Enums/StatusCode.cs
@@ -9,12 +9,13 @@
         Forbidden = 403,
         NotFound = 404,
         RequestTimeout = 408,
+        Rejected = 409,
         Gone = 410,
         UnprocessableEntity = 422,
         InternalServerError = 500,
         NotImplemented = 501,
         ServiceUnavailable = 503,
         Delayed = 999,
-        TooManyRequests = 429
+        TooManyRequests = 429,
     }
 }
---
+++ Runtime/Exceptions/RejectedException.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiplayerProtocol
+{
+    public class RejectedException : RequestErrorResponse
+    {
+        public override StatusCode status => StatusCode.Rejected;
+
+        public RejectedException(string message) : base(message)
+        {
+        }
+    }
+}
