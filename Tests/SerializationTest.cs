5fbfa3a655e08e39f67387c7f5b8c9a6755c434d
HeaX <heax@moonsama.com>
2023-11-30 14:53:27 +0100

    feat: updated test cases

--- Tests/SerializationTest.cs
+++ Tests/SerializationTest.cs
@@ -20,18 +20,26 @@ namespace MultiplayerProtocol.Tests
             {
                 1, 2, 3, 4
             };
+            var testValue2 = "https://test.com";
             var serialized = new SerializedData();
             serialized.Write(testValue);
             serialized.Write(testValue);
+            serialized.Write(testValue2);
+            serialized.Write(testValue2);
             var read = serialized.ReadIntArray();
             var read2 = serialized.ReadIntArray();
+            var read3 = serialized.ReadString();
+            var read4 = serialized.ReadString();
             Debug.Log(
                 "Input: " + string.Join(", ", testValue) + "\n" +
                 "Read 1: " + string.Join(", ", read) + "\n" +
-                "Read 2: " + string.Join(", ", read2)
+                "Read 2: " + string.Join(", ", read2) + "\n" +
+                "Read 3: " + string.Join(", ", read3) + "\n" +
+                "Read 4: " + string.Join(", ", read4)
             );
             Debug.Log("Success: " + (testValue.Length == read.Length && testValue.Length == read2.Length &&
-                                     testValue.Select((v, i) => read[i] == v && read2[i] == v).All(v => v)));
+                                     testValue.Select((v, i) => read[i] == v && read2[i] == v).All(v => v) &&
+                                     testValue2 == read3 && testValue2 == read4));
         }
 
         private void TestDateTime()
